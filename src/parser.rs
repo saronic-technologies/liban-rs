dd4b24a4e936116440e03da60abb3e860ce7f6b2
Joseph Fox-Rabinovitz <joseph.fox-rabinovitz@saronic.com>
2026-05-03 07:59:41 +0000

    fix: this crate is not no-std

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,4 @@
-extern crate alloc;
-use alloc::vec::Vec;
-
-use crate::packet::{Packet, PacketKind};
-use crate::protocol::AnppProtocol;
-
+use crate::{packet::{Packet, PacketKind, PacketLength}, protocol::AnppProtocol};
 use tracing::debug;
 
 #[derive(Debug)]
